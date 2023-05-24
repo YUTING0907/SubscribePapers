4335fe36cc37104023f9d3f8ab733296385fed51
YuTing <yuting9597@163.com>
2023-05-24 15:56:11 +0800

    Create arxiv.py

---
+++ arxiv.py
@@ -0,0 +1,191 @@
+
+'''
+credit to original author: Glenn (chenluda01@outlook.com)
+Author: yuting
+'''
+
+import os
+import requests
+import time
+import json
+import datetime
+from tqdm import tqdm
+
+SERVERCHAN_API_KEY = os.environ.get("SERVERCHAN_API_KEY", None)
+QUERY = os.environ.get('QUERY', 'cs.IR')
+LIMITS = os.environ.get('LIMITS', 3)
+CAIYUN_TOKEN = os.environ.get("CAIYUN_TOKEN", None)
+
+def translate(source, direction='en2zh'):
+    url = "http://api.interpreter.caiyunai.com/v1/translator"
+
+    payload = {
+        "source": source,
+        "trans_type": direction,
+        "request_id": "demo",
+        "detect": True,
+    }
+
+    headers = {
+        "content-type": "application/json",
+        "x-authorization": "token " + CAIYUN_TOKEN,
+    }
+    try:
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+        return json.loads(response.text)["target"]
+    except:
+        return []
+
+def get_yesterday():
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    return yesterday.strftime('%Y-%m-%d')
+
+
+def search_arxiv_papers(search_term, max_results=10):
+    papers = []
+
+    url = f'http://export.arxiv.org/api/query?' + \
+          f'search_query=all:{search_term}' +  \
+          f'&start=0&&max_results={max_results}' + \
+          f'&sortBy=submittedDate&sortOrder=descending'
+
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return []
+
+    feed = response.text
+    entries = feed.split('<entry>')[1:]
+
+    if not entries:
+        return []
+
+    print('[+] 开始处理每日最新论文....')
+
+    for entry in entries:
+
+        title = entry.split('<title>')[1].split('</title>')[0].strip()
+        summary = entry.split('<summary>')[1].split('</summary>')[0].strip()
+        url = entry.split('<id>')[1].split('</id>')[0].strip()
+        pub_date = entry.split('<published>')[1].split('</published>')[0]
+        pub_date = datetime.datetime.strptime(pub_date, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d")
+
+        papers.append({
+            'title': title,
+            'url': url,
+            'pub_date': pub_date,
+            'summary': summary,
+            'translated': '',
+        })
+    
+    print('[+] 开始翻译每日最新论文并缓存....')
+
+    papers = save_and_translate(papers)
+    
+    return papers
+
+
+def send_wechat_message(title, content, SERVERCHAN_API_KEY):
+    url = f'https://sctapi.ftqq.com/{SERVERCHAN_API_KEY}.send'
+    params = {
+        'title': title,
+        'desp': content,
+    }
+    requests.post(url, params=params)
+
+
+def save_and_translate(papers, filename='arxiv.json'):
+    with open(filename, 'r', encoding='utf-8') as f:
+        results = json.load(f)
+
+    cached_title2idx = {result['title'].lower():i for i, result in enumerate(results)}
+    
+    untranslated_papers = []
+    translated_papers = []
+    for paper in papers:
+        title = paper['title'].lower()
+        if title in cached_title2idx.keys():
+            translated_papers.append(
+                results[cached_title2idx[title]]
+            )
+        else:
+            untranslated_papers.append(paper)
+    
+    source = []
+    for paper in untranslated_papers:
+        source.append(paper['summary'])
+    target = translate(source)
+    if len(target) == len(untranslated_papers):
+        for i in range(len(untranslated_papers)):
+            untranslated_papers[i]['translated'] = target[i]
+    
+    results.extend(untranslated_papers)
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(results, f, indent=4, ensure_ascii=False)
+
+    print(f'[+] 总检索条数: {len(papers)} | 命中缓存: {len(translated_papers)} | 实际返回: {len(untranslated_papers)}....')
+
+    return untranslated_papers # 只需要发送缓存中没有的
+
+        
+def cronjob():
+
+    if SERVERCHAN_API_KEY is None:
+        raise Exception("未设置SERVERCHAN_API_KEY环境变量")
+
+    print('[+] 开始执行每日推送任务....')
+
+    yesterday = get_yesterday()
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    print('[+] 开始检索每日最新论文....')
+    papers = search_arxiv_papers(QUERY, LIMITS)
+
+    if papers == []:
+        
+        push_title = f'Arxiv:{QUERY}[X]@{today}'
+        send_wechat_message('', '[WARN] NO UPDATE TODAY!', SERVERCHAN_API_KEY)
+
+        print('[+] 每日推送任务执行结束')
+
+        return True
+        
+
+    print('[+] 开始推送每日最新论文....')
+
+    for ii, paper in enumerate(tqdm(papers, total=len(papers), desc=f"论文推送进度")):
+
+        title = paper['title']
+        url = paper['url']
+        pub_date = paper['pub_date']
+        summary = paper['summary']
+        translated = paper['translated']
+
+        yesterday = get_yesterday()
+
+        if pub_date == yesterday:
+            msg_title = f'[Newest]Title: {title}' 
+        else:
+            msg_title = f'Title: {title}'
+
+        msg_url = f'URL: {url}'
+        msg_pub_date = f'Pub Date：{pub_date}'
+        msg_summary = f'Summary：\n\n{summary}'
+        msg_translated = f'Translated:\n\n{translated}'
+
+        push_title = f'Arxiv:{QUERY}[{ii}]@{today}'
+        msg_content = f"[{msg_title}]({url})\n\n{msg_pub_date}\n\n{msg_url}\n\n{msg_translated}\n\n{msg_summary}\n\n"
+
+        send_wechat_message(push_title, msg_content, SERVERCHAN_API_KEY)
+
+        time.sleep(12)
+
+    print('[+] 每日推送任务执行结束')
+
+    return True
+
+
+if __name__ == '__main__':
+    cronjob()
