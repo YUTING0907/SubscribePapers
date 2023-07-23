1b2964a36a625ebc8c67cd0ee3b9c1d94ea50ece
YuTing <yuting9597@163.com>
2023-07-23 13:03:56 +0800

    Update citer.py

--- citer.py
+++ citer.py
@@ -65,11 +65,12 @@ def update_results_parallel(conf, papers):
     doi_counter = CachedDOICounter()
     with ThreadPoolExecutor(max_workers=5) as executor:
         future_to_paper = {
-            for paper_item in papers:
-                print(f"---in--update_results_parallel-paper_item-:{paper_item}")
             executor.submit(fill_citation, paper_item, doi_counter): paper_item
             for paper_item in papers
         }
+        for paper_item in papers:
+            print(f"---in--update_results_parallel-paper_item-:{paper_item}")
+        
         updated_papers = []
         for future in tqdm(as_completed(future_to_paper), total=len(future_to_paper), desc=f"Updating {conf} papers"):
             paper_item = future_to_paper[future]
